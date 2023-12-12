9a34c4b4875952218842f8ffe0f5ad49823fc2a5
Kipronokirui <victorkipronokirui2@gmail.com>
2023-12-12 22:27:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = (num1*num2)
+let random = Math.random()
+let num3 = 10;
+let num4 = 14;
+const mod = num4%num3;
+const numbers = [10, 15, 20, 5]
+let max = Math.max(...numbers)
